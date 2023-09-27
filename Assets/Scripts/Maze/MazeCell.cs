717ae1a2eace63f444978f33ff2ad254f03cc138
Frozenight <dabaseinas@gmail.com>
2023-09-27 18:37:39 +0300

    Maze

---
+++ Assets/Scripts/Maze/MazeCell.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCell : MonoBehaviour
+{
+    [SerializeField] private GameObject _leftWall;
+    [SerializeField] private GameObject _rightWall;
+    [SerializeField] private GameObject _frontWall;
+    [SerializeField] private GameObject _backWall;
+    [SerializeField] private GameObject _unvisitedBlock;
+
+    public bool isVisited { get; private set; }
+
+    public void Visit()
+    {
+        isVisited = true;
+        _unvisitedBlock.SetActive(false);
+    }
+
+    public void ClearLeft()
+    {
+        _leftWall.SetActive(false);
+    }
+
+    public void ClearRight()
+    {
+        _rightWall.SetActive(false);
+    }
+
+    public void ClearFront()
+    {
+        _frontWall.SetActive(false);
+    }
+
+    public void ClearBack()
+    {
+        _backWall.SetActive(false);
+    }
+
+    public void MarkAsExit()
+    {
+        _leftWall.SetActive(false);
+        _rightWall.SetActive(false);
+        _frontWall.SetActive(false);
+        _backWall.SetActive(false);
+    }
+
+    public void DeleteRandomWall(int x, int y, MazeCell[,] mazeGrid)
+    {
+        // Create an array of walls to choose from
+        GameObject[] walls = { _leftWall, _rightWall, _frontWall, _backWall };
+
+        // Shuffle the array randomly
+        ShuffleArray(walls);
+
+        // Disable the first wall in the shuffled array (randomly chosen)
+        if (walls.Length > 0)
+        {
+            walls[0].SetActive(false);
+            // Determine which neighbor's wall to disable based on the chosen wall
+            if (walls[0] == _leftWall)
+            {
+                // Disable the right wall of the left neighbor
+                if (mazeGrid[x, y - 1] != null)
+                    mazeGrid[x, y - 1]._rightWall.SetActive(false);
+            }
+            else if (walls[0] == _rightWall)
+            {
+                // Disable the left wall of the right neighbor
+                if (mazeGrid[x, y + 1] != null)
+                    mazeGrid[x, y + 1]._leftWall.SetActive(false);
+            }
+            else if (walls[0] == _frontWall)
+            {
+                // Disable the back wall of the front neighbor
+                if (mazeGrid[x - 1, y] != null)
+                    mazeGrid[x - 1, y]._backWall.SetActive(false);
+            }
+            else if (walls[0] == _backWall)
+            {
+                // Disable the front wall of the back neighbor
+                if (mazeGrid[x + 1, y] != null)
+                    mazeGrid[x + 1, y]._frontWall.SetActive(false);
+            }
+        }
+    }
+
+    private void ShuffleArray<T>(T[] array)
+    {
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            T temp = array[i];
+            array[i] = array[randomIndex];
+            array[randomIndex] = temp;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Maze/MazeGenerator.cs
@@ -0,0 +1,172 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MazeGenerator : MonoBehaviour
+{
+    [Header("Maze Parameters")]
+    [SerializeField] private MazeCell _mazeCellPrefab;
+    [SerializeField] private int _mazeWidth;
+    [SerializeField] private int _mazeHeight;
+    [SerializeField] private float mazeSpeed = 0.02f;
+
+    [Header("Loops Parameters")]
+    [SerializeField] private bool allowLoops = false;
+    [SerializeField] private int numberOfLoops = 5; // Set the number of loops you want
+
+    private MazeCell[,] mazeGrid;
+    private int cellSize = 4;
+
+    private IEnumerator Start()
+    {
+        mazeGrid = new MazeCell[_mazeWidth, _mazeHeight];
+        for (int i = 0; i < _mazeWidth; i++)
+        {
+            for (int j = 0; j < _mazeHeight; j++)
+            {
+                mazeGrid[i, j] = Instantiate(_mazeCellPrefab, new Vector2(i * cellSize, j * cellSize), Quaternion.identity);
+            }
+        }
+
+        // Set the exit cell
+        MazeCell exitCell = mazeGrid[_mazeWidth - 1, _mazeHeight - 2];
+
+        yield return GenerateMaze(null, mazeGrid[0, 0], exitCell); // Pass the exit cell
+
+
+        // Randomly delete walls to create loops only once
+        if (allowLoops)
+        {
+            CreateLoops();
+        }
+    }
+
+
+    private IEnumerator GenerateMaze(MazeCell previousCell, MazeCell currentCell, MazeCell exitCell)
+    {
+        currentCell.Visit();
+        ClearWalls(previousCell, currentCell);
+
+        yield return new WaitForSeconds(mazeSpeed);
+
+        MazeCell nextCell;
+
+        do
+        {
+            nextCell = GetNextUnvisitedCell(currentCell);
+
+            if (nextCell != null)
+            {
+                yield return GenerateMaze(currentCell, nextCell, exitCell);
+            }
+        } while (nextCell != null);
+
+        // Check if the current cell is the exit cell, and mark it as the exit
+        if (currentCell == exitCell)
+        {
+            currentCell.MarkAsExit();
+        }
+    }
+
+
+
+    private MazeCell GetNextUnvisitedCell(MazeCell currentCell)
+    {
+        var unvisitedCells = GetUnvisitedCells(currentCell);
+
+        return unvisitedCells.OrderBy(_ => Random.Range(1, 10)).FirstOrDefault();
+
+    }
+
+    private IEnumerable<MazeCell> GetUnvisitedCells(MazeCell currentCell)
+    {
+        int x = (int)currentCell.transform.position.x / cellSize;
+        int y = (int)currentCell.transform.position.y / cellSize;
+
+        if (x + 1 < _mazeWidth)
+        {
+            var cellToRight = mazeGrid[x + 1, y];
+            if (cellToRight.isVisited == false)
+                yield return cellToRight;
+        }
+
+        if (x - 1  >= 0)
+        {
+            var cellToLeft = mazeGrid[x - 1, y];
+            if (!cellToLeft.isVisited)
+                yield return cellToLeft;
+        }
+
+        if (y + 1 < _mazeHeight)
+        {
+            var CellToFront = mazeGrid[x, y + 1];
+            if (!CellToFront.isVisited)
+                yield return CellToFront;
+        }
+
+        if (y - 1 >= 0)
+        {
+            var CellToBack = mazeGrid[x, y - 1];
+            if (!CellToBack.isVisited)
+                yield return CellToBack;
+        }    
+    }
+
+    private void CreateLoops()
+    {
+        int loopsCreated = 0;
+
+        while (loopsCreated < numberOfLoops)
+        {
+            int randomX = Random.Range(1, _mazeWidth - 1);
+            int randomY = Random.Range(1, _mazeHeight - 1);
+
+            var randomCell = mazeGrid[randomX, randomY];
+            //Debug.Log(mazeGrid[randomX, randomY]);
+            // Check if the cell is not on the edge of the maze
+            if (randomCell != null)
+            {
+                // Implement logic to delete a wall in a way that creates a loop
+                // For example, you can choose a random wall within the cell and delete it
+                // Be sure to update your MazeCell class to handle wall deletion
+                randomCell.DeleteRandomWall(randomX, randomY, mazeGrid);
+                loopsCreated++;
+            }
+        }
+    }
+
+
+    private void ClearWalls(MazeCell previousCell, MazeCell currentCell)
+    {
+        if (previousCell == null)
+            return;
+
+        if (previousCell.transform.position.x < currentCell.transform.position.x)
+        {
+            previousCell.ClearRight();
+            currentCell.ClearLeft();
+            return;
+        }
+        if (previousCell.transform.position.x > currentCell.transform.position.x)
+        {
+            previousCell.ClearLeft();
+            currentCell.ClearRight();
+            return;
+        }
+
+        if (previousCell.transform.position.y < currentCell.transform.position.y)
+        {
+            previousCell.ClearFront();
+            currentCell.ClearBack();
+            return;
+        }
+
+        if (previousCell.transform.position.y > currentCell.transform.position.y)
+        {
+            previousCell.ClearBack();
+            currentCell.ClearFront();
+            return;
+        }
+    }
+}
