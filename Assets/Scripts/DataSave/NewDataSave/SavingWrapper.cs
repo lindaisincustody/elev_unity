9f16c1f37c0e2d98c78315a064e2e4cbe55330fb
Frozenight <dabaseinas@gmail.com>
2024-04-15 21:41:51 +0300

    Main menu + item loading/saving fixed

--- Assets/Scripts/DataSave/NewDataSave/SavingWrapper.cs
+++ Assets/Scripts/DataSave/NewDataSave/SavingWrapper.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class SavingWrapper : MonoBehaviour
 {
@@ -26,17 +28,46 @@ public class SavingWrapper : MonoBehaviour
 
     public void SaveInventory(InventoryData inventoryData)
     {
-        string json = JsonUtility.ToJson(inventoryData);
+        List<string> itemIdList = new List<string>();
+        foreach (var item in inventoryData.items)
+        {
+            itemIdList.Add(item.itemId);
+        }
+
+        // Serialize the itemIdList using Newtonsoft.Json
+        string json = JsonConvert.SerializeObject(itemIdList);
+
+        // Write the JSON string to a file
         File.WriteAllText(Path.Combine(Application.persistentDataPath, inventorySaveFileName), json);
     }
 
+
     public InventoryData LoadInventory()
     {
+        ShopItem[] shopItems = Resources.LoadAll<ShopItem>("Items");
+
         string path = Path.Combine(Application.persistentDataPath, inventorySaveFileName);
         if (File.Exists(path))
         {
             string json = File.ReadAllText(path);
-            return JsonUtility.FromJson<InventoryData>(json);
+            List<string> itemIdList = JsonConvert.DeserializeObject<List<string>>(json); // Deserialize using JsonConvert
+
+            InventoryData inventoryData = new InventoryData();
+            foreach (var itemId in itemIdList)
+            {
+                // Find the ShopItem with the corresponding itemId
+                ShopItem item = Array.Find(shopItems, x => x.itemId == itemId);
+                if (item != null)
+                {
+                    inventoryData.items.Add(item);
+                }
+                else
+                {
+                    Debug.LogWarning("ShopItem with itemId " + itemId + " not found in Resources.");
+                }
+            }
+
+            return inventoryData;
         }
 
         return new InventoryData();
