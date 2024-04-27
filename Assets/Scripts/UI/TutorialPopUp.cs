cd14f67436bdf0e81b1e960a816a26f0b642faf3
Dainius <dabaseinas@gmail.com>
2024-04-27 20:42:46 +0300

    Tutorial explanation system

---
+++ Assets/Scripts/UI/TutorialPopUp.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TutorialPopUp : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] GameObject BackgroundIamge;
+    private float closeDelay = 3f;
+    private Coroutine coroutine;
+    private string boxText;
+
+    private void Start()
+    {
+        boxText = text.text;    
+    }
+
+    public void ActivatePopUp()
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+
+        if (BackgroundIamge.activeSelf)
+            return;
+
+        BackgroundIamge.SetActive(true);
+
+        StartCoroutine(TypeMessage(boxText));
+    }
+
+    private IEnumerator TypeMessage(string message)
+    {
+        text.text = ""; 
+        text.gameObject.SetActive(true);
+        string[] words = message.Split(' ');
+        foreach (string word in words)
+        {
+            if (word.Contains("color"))
+            {
+                text.text += word + " ";
+            }
+            else
+            {
+                foreach (char letter in word)
+                {
+                    text.text += letter;
+                    yield return new WaitForSeconds(0.01f);
+                }
+                text.text += " ";
+            }
+        }
+    }
+
+    public void DeactivatePopUp()
+    {
+        coroutine = StartCoroutine(ClosePopUp());
+    }
+
+    private IEnumerator ClosePopUp()
+    {
+        yield return new WaitForSeconds(closeDelay);
+        BackgroundIamge.SetActive(false);
+        text.gameObject.SetActive(false);
+    }
+}
