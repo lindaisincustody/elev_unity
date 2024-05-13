ca2644d9ca44ea2282f6a1ba58edaa74e376b8ee
lindaisincustody <39005386+lindaisincustody@users.noreply.github.com>
2024-05-13 23:44:51 +0300

    UI fixes

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -124,6 +124,7 @@ public class Player : MonoBehaviour
 
     private void OpenPoemBook()
     {
+        poemAvailable.SetActive(false);
         if (playerData.currentLevel > playerData.poemsUsed)
         {
             int poemToOpen = playerData.currentLevel;
